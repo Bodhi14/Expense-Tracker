151f5522d24bda9583ca039bcddff9ac290a7ee5
Bodhi14 <bodhi140802@gmail.com>
2022-05-29 22:57:58 +0530

    App Changed

--- src/App.js
+++ src/App.js
@@ -8,42 +8,43 @@ function App()
 {  
     let expenses = [
         {
-            id: 'e1',
-            title: "College Fee",
+            id: '1',
+            title: "College Fee ",
             amount: 250,
             date: new Date(2022, 4, 28)
-
-
+    
+    
         },
         {
-            id: 'e2',
-            title: "College Fee 2",
+            id: '2',
+            title: "College Fee ",
             amount: 500,
             date: new Date(2022, 4, 28)
-
-
+    
+    
         },
         {
-            id: 'e3',
-            title: "College Fee 3",
+            id: '3',
+            title: "College Fee ",
             amount: 750,
             date: new Date(2022, 4, 28)
-
-
+    
+    
         },
         {
-            id: 'e4',
-            title: "College Fee 4",
+            id: '4',
+            title: "College Fee ",
             amount: 1000,
             date: new Date(2022, 4, 28)
-
-
+    
+    
         }
     ]
+    
 
     return(
         <div>
-        <Expenses />
+        <Expenses item={ expenses }/>
        </div>
     );
 }
