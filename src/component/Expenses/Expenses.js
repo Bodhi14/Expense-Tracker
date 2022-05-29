9b18b73f2c1035c7b0b19f8599eacdfb1048bb32
Bodhi14 <bodhi140802@gmail.com>
2022-05-29 22:59:14 +0530

    Expenses Page changed

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -2,47 +2,13 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 
-let expenses = [
-    {
-        id: '1',
-        title: "College Fee ",
-        amount: 250,
-        date: new Date(2022, 4, 28)
 
-
-    },
-    {
-        id: '2',
-        title: "College Fee ",
-        amount: 500,
-        date: new Date(2022, 4, 28)
-
-
-    },
-    {
-        id: '3',
-        title: "College Fee ",
-        amount: 750,
-        date: new Date(2022, 4, 28)
-
-
-    },
-    {
-        id: '4',
-        title: "College Fee ",
-        amount: 1000,
-        date: new Date(2022, 4, 28)
-
-
-    }
-]
-
-const value = (i) =>
-{
-    let ans = expenses[i].amount*expenses[i].id;
+const value = (props, i) =>
+ {
+    let ans = (props.item)[i].amount*(props.item)[i].id;
     if(ans>=2000)
     {
-        expenses[i].title = "(Tax Applicable) College Fee ";
+        (props.item[i]).title = "(Tax Applicable) College Fee ";
         ans*=1.3;
 
     }
@@ -56,13 +22,12 @@ const Expenses = (props) =>
     
    return(
     <>
-    <ExpenseItem date={expenses[0].date} title={expenses[0].title + expenses[0].id} amount={value(0)}></ExpenseItem>
-    <ExpenseItem date={expenses[1].date} title={expenses[1].title + expenses[1].id} amount={value(1)}></ExpenseItem>
-    <ExpenseItem date={expenses[2].date} title={expenses[2].title + expenses[2].id} amount={value(2)}></ExpenseItem>
-    <ExpenseItem date={expenses[3].date} title={expenses[3].title + expenses[3].id} amount={value(3)}></ExpenseItem>
-    </> 
+    <ExpenseItem date={props.item[0].date} title={props.item[0].title + props.item[0].id} amount={value(props, 0)}></ExpenseItem>
+    <ExpenseItem date={props.item[1].date} title={props.item[1].title + props.item[1].id} amount={value(props, 1)}></ExpenseItem>
+    <ExpenseItem date={props.item[2].date} title={props.item[2].title + props.item[2].id} amount={value(props, 2)}></ExpenseItem>
+    <ExpenseItem date={props.item[3].date} title={props.item[3].title + props.item[3].id} amount={value(props, 3)}></ExpenseItem>
+    </>
    );
-
 }
 
 export default Expenses;
