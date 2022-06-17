d0e4dcfe39b005b212004e54de9ce00133bd94ef
Bodhi14 <bodhi140802@gmail.com>
2022-06-18 00:11:51 +0530

    Shows list of all expenses on page load

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import './ExpenseForm.css';
+import * as Yup from "yup";
 import { useFormik } from "formik";
 
 
@@ -8,27 +9,45 @@ import { useFormik } from "formik";
 
 const ExpenseForm = (props) => {
 
-    const validation = (values) => {
-        const errors = {}
-        if(!values.id){
-            errors.id = 'ID is a Required Field';
-        }
-        else if (values.id<0)
-        {
-            errors.id = "Sorry, ID can't be negative";
-        }
-    }
-
+    
     const formik = useFormik({
     initialValues:{
-        title: "",
-        amount: "",   
-        id: ""
+
+        title: '',
+        amount: '',   
+        id: '' 
+
     },
-    validation,
+    validateOnBlur: true,
     onSubmit: values => {
-        alert("New Expense added having details: " + JSON.stringify(values, null, 2));
-    }
+        const ExpenseData = {
+            title: formik.values.title,
+            amount: formik.values.amount,
+            id: formik.values.id
+
+        }
+
+        formik.values.title = '';
+        formik.values.amount = '';
+        formik.values.id = '';
+
+        props.onSaveExpenseData(ExpenseData);
+        fetch('http://127.0.0.1:8000/expenses-api/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(ExpenseData)
+     }).then(() => {
+                alert("New Expense added , having details id : " + JSON.stringify(ExpenseData.id)+ ", \n title : " + JSON.stringify(ExpenseData.title) + ", \n amount : " + JSON.stringify(ExpenseData.amount));
+
+     }).catch(() => {
+        alert("Sorry, due to some error, this item can't be added");
+    });
+    },
+    validationSchema: Yup.object({
+         title: Yup.string().max(10, "Can be at max 10 characters long").required("Required"),
+         id: Yup.number().required("Required"),
+
+    }),
 });
 
 //     const [newtitle, setnewtitle] = useState('');
@@ -103,20 +122,17 @@ const ExpenseForm = (props) => {
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" name="title" value={ formik.values.title } onChange={ formik.handleChange }/>
-                
+                <input type="text" id="title" name="title" value={ formik.values.title } onChange={ formik.handleChange } onBlur={ formik.handleBlur }/>
+                {(formik.touched.title && formik.errors.title) ? <p className="errortext">{formik.errors.title}</p> : null}
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" name="amount" value={ formik.values.amount } min="0.01" step="0.01" onChange={ formik.handleChange }/>
+                <input type="number" id="amount" name="amount" value={ formik.values.amount } min="0.01" step="0.01" onChange={ formik.handleChange } onBlur={ formik.handleBlur }/>
             </div>
             <div className="new-expense__control">
                 <label>ID</label>
-                <input type="number" name="id" value={ formik.values.id } onChange={ formik.handleChange } />
-                { (formik.errors.id) ?
-                    <div>{formik.errors.id}</div>
-                    : null
-                }
+                <input type="number" id="id" name="id" value={ formik.values.id } onChange={ formik.handleChange } onBlur={ formik.handleBlur } />
+                {(formik.touched.id && formik.errors.id) ? <p className="errortext">{formik.errors.id}</p> : null}
             </div>
         </div>
         <div className="new-expense__actions">
