274c7a43ced2370074fc42b3570b25dd8fe383bf
Bodhi14 <bodhi140802@gmail.com>
2022-06-17 21:17:25 +0530

    Used Formik

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,94 +1,122 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import './ExpenseForm.css';
+import { useFormik } from "formik";
 
 
 
 
 const ExpenseForm = (props) => {
 
-    const [newtitle, setnewtitle] = useState('');
-
-    const [newamount, setnewamount] = useState('');
-
-
-    const[newid, setnewid] = useState('');
-
-
-
-    
-
-const titlehandler = (event) =>
-{
-    setnewtitle(event.target.value);
-
-    
-
-}
-
-const amounthandler = (event) =>
-{
-    setnewamount(event.target.value);
-
-}
-
-const idhandler = (event) =>
-{
-    setnewid(event.target.value);
-}
-
-
-
-const SubmitHandler = (event) =>
-{
-    event.preventDefault();
-
-    const ExpenseData = {
-        title: newtitle,
-        amount: newamount,
-        id: newid
-
+    const validation = (values) => {
+        const errors = {}
+        if(!values.id){
+            errors.id = 'ID is a Required Field';
+        }
+        else if (values.id<0)
+        {
+            errors.id = "Sorry, ID can't be negative";
+        }
     }
 
-    setnewtitle('');
-    setnewamount('');
-    setnewid('');
+    const formik = useFormik({
+    initialValues:{
+        title: "",
+        amount: "",   
+        id: ""
+    },
+    validation,
+    onSubmit: values => {
+        alert("New Expense added having details: " + JSON.stringify(values, null, 2));
+    }
+});
+
+//     const [newtitle, setnewtitle] = useState('');
+
+//     const [newamount, setnewamount] = useState('');
+
+
+//     const[newid, setnewid] = useState('');
+
+
+
+    
+
+// const titlehandler = (event) =>
+// {
+//     setnewtitle(event.target.value);
+
+    
+
+// }
+
+// const amounthandler = (event) =>
+// {
+//     setnewamount(event.target.value);
+
+// }
+
+// const idhandler = (event) =>
+// {
+//     setnewid(event.target.value);
+// } 
+
+
+
+// const SubmitHandler = (event) =>
+// {
+//     event.preventDefault();
+
+//     const ExpenseData = {
+//         title: newtitle,
+//         amount: newamount,
+//         id: newid
+
+//     }
+
+//     setnewtitle('');
+//     setnewamount('');
+//     setnewid('');
 
     
 
 
-    props.onSaveExpenseData(ExpenseData);
+//     props.onSaveExpenseData(ExpenseData);
 
 
-    fetch('http://127.0.0.1:8000/expenses-api/', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(ExpenseData)
-    }).then(() => {
-               setTimeout( () => { document.querySelector("button").innerText = "Adding Expense..." }, 1000);
-               alert("New Expense added , having id " + ExpenseData.id);
+//     fetch('http://127.0.0.1:8000/expenses-api/', {
+//         method: 'POST',
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(ExpenseData)
+//     }).then(() => {
+//                alert("New Expense added , having id " + ExpenseData.id);
 
-    }).catch(() => {
-        alert("Sorry, due to some error, this item can't be added");
-    })
-}
+//     }).catch(() => {
+//         alert("Sorry, due to some error, this item can't be added");
+//     });
+// }
 
 
 
     return(
-        <form onSubmit={ SubmitHandler }>
+        <form onSubmit={ formik.handleSubmit }>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" value={ newtitle } onChange={ titlehandler }/>
+                <input type="text" name="title" value={ formik.values.title } onChange={ formik.handleChange }/>
+                
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" value={ newamount } min="0.01" step="0.01" onChange={ amounthandler }/>
+                <input type="number" name="amount" value={ formik.values.amount } min="0.01" step="0.01" onChange={ formik.handleChange }/>
             </div>
             <div className="new-expense__control">
                 <label>ID</label>
-                <input type="number" value={ newid } onChange={ idhandler } />
+                <input type="number" name="id" value={ formik.values.id } onChange={ formik.handleChange } />
+                { (formik.errors.id) ?
+                    <div>{formik.errors.id}</div>
+                    : null
+                }
             </div>
         </div>
         <div className="new-expense__actions">
