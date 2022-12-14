960b16c43dc9f37d3d4fe793af1bd00a6556ece2
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2022-12-15 03:01:20 +0530

    Updated API

--- src/App.js
+++ src/App.js
@@ -1,61 +1,36 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import './App.css';
+import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
+let prevexpenses = [];
 
+const App = () => {
+  const [expenses, setnewexpenses] = useState(prevexpenses);
 
-let prevexpenses = []
+  const addexpensehandler = (expense) => {
+    const updatedexpense = [expense, ...expenses];
 
-const App = () =>
-{  
-    
-    const [expenses, setnewexpenses] = useState(prevexpenses);
+    setnewexpenses(updatedexpense);
+  };
 
-    
+  useEffect((expenses) => {
+    fetch("bodhi.pythonanywhere.com/expenses-api/")
+      .then((res) => res.json())
+      .then((data) => {
+        setnewexpenses(data);
+        console.log(data);
+      });
+  }, []);
 
-
-    
-
-
-    const addexpensehandler = (expense) =>
-    {
-
-        
-
-        const updatedexpense = [expense, ...expenses];
-        
-        setnewexpenses(updatedexpense);
-        
-
-    }
-
-    
-
-    useEffect((expenses) => {
-
-        fetch('http://127.0.0.1:8000/expenses-api/').then(
-            res => res.json()
-            ).then((data) => {
-            
-                setnewexpenses(data)
-                console.log(data)
-              
-              })
-        
-        
-        }, []);
-    
-
-    return(
-       <div>
-        <NewExpense addexpense={ addexpensehandler } />
-        <Expenses item={ expenses }/>
-       </div>
-    );
-}
+  return (
+    <div>
+      <NewExpense addexpense={addexpensehandler} />
+      <Expenses item={expenses} />
+    </div>
+  );
+};
 
 export default App;
-
--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,102 +1,83 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import './ExpenseForm.css';
-
-
-
+import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
+  const [newtitle, setnewtitle] = useState("");
 
-    const [newtitle, setnewtitle] = useState('');
+  const [newamount, setnewamount] = useState("");
 
-    const [newamount, setnewamount] = useState('');
+  const [newid, setnewid] = useState("");
 
-
-    const[newid, setnewid] = useState('');
-
-
-
-    
-
-const titlehandler = (event) =>
-{
+  const titlehandler = (event) => {
     setnewtitle(event.target.value);
+  };
 
-    
-
-}
-
-const amounthandler = (event) =>
-{
+  const amounthandler = (event) => {
     setnewamount(event.target.value);
+  };
 
-}
-
-const idhandler = (event) =>
-{
+  const idhandler = (event) => {
     setnewid(event.target.value);
-}
+  };
 
-
-
-const SubmitHandler = (event) =>
-{
+  const SubmitHandler = (event) => {
     event.preventDefault();
 
     const ExpenseData = {
-        title: newtitle,
-        amount: newamount,
-        id: newid
+      title: newtitle,
+      amount: newamount,
+      id: newid
+    };
 
-    }
-
-    setnewtitle('');
-    setnewamount('');
-    setnewid('');
-
-    
-
+    setnewtitle("");
+    setnewamount("");
+    setnewid("");
 
     props.onSaveExpenseData(ExpenseData);
 
-
-    fetch('http://127.0.0.1:8000/expenses-api/', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(ExpenseData)
-    }).then(() => {
-               alert("New Expense added , having id " + ExpenseData.id);
-    }).catch(() => {
-        alert("Sorry, due to some error, this item can't be added");
+    fetch("bodhi.pythonanywhere.com/expenses-api/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(ExpenseData)
     })
-}
+      .then(() => {
+        alert("New Expense added , having id " + ExpenseData.id);
+      })
+      .catch(() => {
+        alert("Sorry, due to some error, this item can't be added");
+      });
+  };
 
-
-
-    return(
-        <form onSubmit={ SubmitHandler }>
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type="text" value={ newtitle } onChange={ titlehandler }/>
-            </div>
-            <div className="new-expense__control">
-                <label>Amount</label>
-                <input type="number" value={ newamount } min="0.01" step="0.01" onChange={ amounthandler }/>
-            </div>
-            <div className="new-expense__control">
-                <label>ID</label>
-                <input type="number" value={ newid } onChange={ idhandler } />
-            </div>
+  return (
+    <form onSubmit={SubmitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" value={newtitle} onChange={titlehandler} />
         </div>
-        <div className="new-expense__actions">
-            <button className="btn btn-primary" id="btn" type="submit">Add Expense</button>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            value={newamount}
+            min="0.01"
+            step="0.01"
+            onChange={amounthandler}
+          />
         </div>
-        </form>
-
-
-    );
-
-}
+        <div className="new-expense__control">
+          <label>ID</label>
+          <input type="number" value={newid} onChange={idhandler} />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button className="btn btn-primary" id="btn" type="submit">
+          Add Expense
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default ExpenseForm;
