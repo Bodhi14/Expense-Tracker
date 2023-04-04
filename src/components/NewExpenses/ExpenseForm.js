32163e9175ea26cf52e7f257ce397f1ca77159db
Bodhisattwa Das <bodhi140802@gmail.com>
2023-04-04 11:13:47 +0530

    Update

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,112 +1,73 @@
-/* eslint-disable no-unused-vars */
-import axios from "axios";
-import React, { useState } from "react";
-import "./ExpenseForm.css";
+import React from "react";
+import './ExpenseForm.css';
+import * as Yup from "yup";
+import { useFormik } from "formik";
 
 const ExpenseForm = (props) => {
+    const formik = useFormik({
+    initialValues:{
+        title: '',
+        amount: '',   
+        id: ''
+    },
 
-  const postURL = "https://bodhi.pythonanywhere.com/expenses-api/";
-  
-  const [newtitle, setnewtitle] = useState("");
+    validateOnBlur: true,
+    onSubmit: values => {
+        const ExpenseData = {
+            title: formik.values.title,
+            amount: formik.values.amount,
+            id: formik.values.id
 
-  const [newamount, setnewamount] = useState("");
+        }
 
-  const [newid, setnewid] = useState("");
+        formik.values.title = '';
+        formik.values.amount = '';
+        formik.values.id = '';
 
-  const titlehandler = (event) => {
-    setnewtitle(event.target.value);
-  };
-
-  const amounthandler = (event) => {
-    setnewamount(event.target.value);
-  };
-
-  const idhandler = (event) => {
-    setnewid(event.target.value);
-  };
-
-  const [active, setactive] = useState(false);
-
-  const SubmitHandler = (event) => {
-    event.preventDefault();
-
-    const ExpenseData = {
-      title: newtitle,
-      amount: newamount,
-      id: newid
-    };
-
-    
-    props.onSaveExpenseData(ExpenseData);
-
-    setnewtitle("");
-    setnewamount("");
-    setnewid("");
-
-
-    axios.post(postURL, {
-      title: ExpenseData.title,
-      amount: ExpenseData.amount,
-      id: ExpenseData.id
-    })
-      .then(() => {
-        alert("New Expense added , having id " + ExpenseData.id);
-      })
-      .catch(() => {
+        props.onSaveExpenseData(ExpenseData);
+        fetch('https://bodhi.pythonanywhere.com/expenses-api/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(ExpenseData)
+     }).then(() => {
+                alert("New Expense added , having details id : " + JSON.stringify(ExpenseData.id)+ ", \n title : " + JSON.stringify(ExpenseData.title) + ", \n amount : " + JSON.stringify(ExpenseData.amount));
+     }).catch(() => {
         alert("Sorry, due to some error, this item can't be added");
-      });
-  };
+    });
+    },
+    validationSchema: Yup.object({
+         title: Yup.string().max(20, "Can be at max 20 characters long").required("Required"),
+         id: Yup.number().required("Required"),
+    }),
+});
 
-  return (
-    <form onSubmit={SubmitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" value={newtitle} onChange={titlehandler} />
+
+
+    return(
+        <form onSubmit={ formik.handleSubmit }>
+        <div className="new-expense__controls">
+            <div className="new-expense__control">
+                <label>Title</label>
+                <input type="text" id="title" name="title" value={ formik.values.title } onChange={ formik.handleChange } onBlur={ formik.handleBlur }/>
+                {(formik.touched.title && formik.errors.title) ? <p className="errortext">{formik.errors.title}</p> : null}
+            </div>
+            <div className="new-expense__control">
+                <label>Amount</label>
+                <input type="number" id="amount" name="amount" value={ formik.values.amount } min="0.01" step="0.01" onChange={ formik.handleChange } onBlur={ formik.handleBlur }/>
+            </div>
+            <div className="new-expense__control">
+                <label>ID</label>
+                <input type="number" id="id" name="id" value={ formik.values.id } onChange={ formik.handleChange } onBlur={ formik.handleBlur } />
+                {(formik.touched.id && formik.errors.id) ? <p className="errortext">{formik.errors.id}</p> : null}
+            </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            value={newamount}
-            min="0.01"
-            step="0.01"
-            onChange={amounthandler}
-          />
+        <div className="new-expense__actions">
+            <button className="btn btn-primary" id="btn" type="submit">Add Expense</button>
+            
         </div>
-        <div className="new-expense__control">
-          <label>ID</label>
-          <input type="number" value={newid} onChange={idhandler} />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button
-          className="btn color-btn"
-          id="btn"
-          type="submit"
-          onClick={() => {
-            setTimeout(() => {
-              setactive(false);
-            }, 1000);
-            setactive(true);
-          }}
-          style={{ backgroundColor: active ? "green" : "purple" }}
-          onMouseEnter={()=>
-               {
-                setactive(true);
-              }
-            }
-          onMouseLeave={()=>
-             {
-              setactive(false);
-             }
-          }
-        >
-          Add Expense
-        </button>
-      </div>
-    </form>
-  );
-};
+        </form>
+    );
+
+}
 
 export default ExpenseForm;
